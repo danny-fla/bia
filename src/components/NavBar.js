fd6568c8e1d107112747b57a5c2ed1d1f52dbbb2
danny-fla <dannyfla@live.ie>
2024-01-20 16:05:17 +0000

    create basic navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/bia-logo.png"
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
           <img src={logo} alt="logo" height="60" />
@@ -13,13 +14,13 @@ const NavBar = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <Nav.Link>
-              <i className="fas fa-home"></i>Home
+            <i className="fa-solid fa-house"></i>
             </Nav.Link>
             <Nav.Link>
-              <i className="fas fa-sign-in-alt"></i>Sign in
+              <i className="fas fa-sign-in-alt"></i>
             </Nav.Link>
             <Nav.Link>
-              <i className="fas fa-user-plus"></i>Sign up
+              <i className="fas fa-user-plus"></i>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>

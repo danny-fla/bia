0593e940f9ba29a27f5606f4e3125a785b1d809d
danny-fla <dannyfla@live.ie>
2024-01-20 17:12:55 +0000

    install react router and add to index.js add a render prop to Route add navlink components to navigation handle incorrect urls

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,26 +2,43 @@ import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/bia-logo.png"
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
+      <NavLink to="/">
         <Navbar.Brand>
           <img src={logo} alt="logo" height="60" />
         </Navbar.Brand>
+        </NavLink>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
-            <i className="fa-solid fa-house"></i>
-            </Nav.Link>
-            <Nav.Link>
-              <i className="fas fa-sign-in-alt"></i>
-            </Nav.Link>
-            <Nav.Link>
-              <i className="fas fa-user-plus"></i>
-            </Nav.Link>
+          <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fas fa-home"></i>Home
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </NavLink>
+            <NavLink
+              to="/signup"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
+              <i className="fas fa-user-plus"></i>Sign up
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>

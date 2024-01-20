e49888b6b8ee54e62b4ea75000083e68a4c6882a
danny-fla <dannyfla@live.ie>
2024-01-20 10:29:32 +0000

    install React Bootstrap and valid its connection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Button from "react-bootstrap/Button";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1> test</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }

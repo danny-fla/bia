d74bbe13af1ed96350050dfb9887aa9694a913f2
danny-fla <dannyfla@live.ie>
2024-01-25 12:24:28 +0000

    create new files for: recipecreateform.js/ asset.module.css/ recipecreateeditform.module.css

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -8,6 +8,7 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
+import signin from "../../assets/sign-in.png"
 
 import { Link, useHistory } from "react-router-dom";
 
@@ -49,9 +50,20 @@ function SignInForm() {
 
   return (
     <Row className={styles.Row}>
-      <Col className="my-auto p-0 p-md-2" md={6}>
+      <Col
+        md={4}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
+      >
+        <Image
+          className={`${appStyles.FillerImage}`}
+          src={signin}
+        />
+      </Col>
+      <Col className="my-auto p-0 p-md-2" md={8}>
         <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>sign in</h1>
+        <h1 className={styles.Header}>Welcome back</h1>
+          <br></br>
+          <h3 className={styles.SubHeader}>sign in</h3>
           <Form onSubmit={handleSubmit}>
             <Form.Group controlId="username">
               <Form.Label className="d-none">Username</Form.Label>
@@ -105,15 +117,7 @@ function SignInForm() {
           </Link>
         </Container>
       </Col>
-      <Col
-        md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
-      >
-        <Image
-          className={`${appStyles.FillerImage}`}
-          src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero.jpg"}
-        />
-      </Col>
+      
     </Row>
   );
 }

37cd8fcafa40327b39f17a15b02e085d50309b80
danny-fla <dannyfla@live.ie>
2024-02-01 18:03:20 +0000

    follow add follow functionality between profiles

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -5,6 +5,9 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { Button } from "react-bootstrap";
+import { useSetProfileData } from "../../contexts/ProfileDataContext";
+
+
 
 const Profile = (props) => {
   const { profile, mobile, imageSize = 55 } = props;
@@ -12,7 +15,7 @@ const Profile = (props) => {
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
-
+  const {handleFollow} = useSetProfileData();
   return (
     <div
       className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
@@ -39,7 +42,7 @@ const Profile = (props) => {
           ) : (
             <Button
               className={`${btnStyles.Button} ${btnStyles.Black}`}
-              onClick={() => {}}
+              onClick={() => handleFollow(profile)}
             >
               follow
             </Button>

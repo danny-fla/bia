311b5d1c58f4fd3b3d0c9b7d15b3c06c40d7f1f5
danny-fla <dannyfla@live.ie>
2024-02-01 10:01:07 +0000

    implement infinite scroll and search bar input

--- src/pages/recipes/RecipesPage.js
+++ src/pages/recipes/RecipesPage.js
@@ -14,16 +14,20 @@ import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 
 import NoResults from "../../assets/no-results.png";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
 
 function RecipesPage({ message, filter = "" }) {
   const [recipes, setRecipes] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
 
+  const [query, setQuery] = useState("");
+
   useEffect(() => {
     const fetchRecipes = async () => {
       try {
-        const { data } = await axiosReq.get(`/recipe/?${filter}`);
+        const { data } = await axiosReq.get(`/recipe/?${filter}search=${query}`);
         setRecipes(data);
         setHasLoaded(true);
       } catch (err) {
@@ -32,19 +36,43 @@ function RecipesPage({ message, filter = "" }) {
     };
 
     setHasLoaded(false);
-    fetchRecipes();
-  }, [filter, pathname]);
+    const timer = setTimeout(() => {
+        fetchRecipes();
+    }, 1000)
+    return () => {
+        clearTimeout(timer)
+    }
+  }, [filter, query, pathname]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search bia"
+          />
+        </Form>
         {hasLoaded ? (
           <>
             {recipes.results.length ? (
-              recipes.results.map((recipe) => (
+              <InfiniteScroll
+              children={recipes.results.map((recipe) => (
                 <Recipe key={recipe.id} {...recipe} setRecipes={setRecipes} />
-              ))
+              ))}
+              dataLength={recipes.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!recipes.next}
+              next={() => fetchMoreData(recipes, setRecipes)}
+            />
             ) : (
               <Container className={appStyles.Content}>
                 <Asset src={NoResults} message={message} />
